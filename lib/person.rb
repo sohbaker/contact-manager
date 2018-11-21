82f01935b09def752615fae8ed4a321aaee539a9
Tola's Terminal <sohbaker@gmail.com>
2018-11-21 16:52:42 +0000

    minor amends to person class. arguments match instance variables

--- lib/person.rb
+++ lib/person.rb
@@ -2,16 +2,16 @@ class Person
   attr_reader :first_name, :surname, :email, :phonenumber
 
   def initialize(f_name, s_name, email, phonenumber)
-    @first_name = f_name
-    @surname = s_name
+    @f_name = f_name
+    @s_name = s_name
     @email = email
     @phonenumber = phonenumber
   end
 
   def phonebook_entry
     {
-      :fname => @first_name,
-      :sname => @surname,
+      :fname => @f_name,
+      :sname => @s_name,
       :email => @email,
       :mobile => @phonenumber
     }
